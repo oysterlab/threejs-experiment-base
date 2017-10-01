1a85d10cdd69332abd1ea0ca64bd96a94e438a74
oysterlab <oysterlaboratory@gmail.com>
2017-10-01 22:55:04 +0900

    glslify

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 const THREE = require('three')
 const OrbitControls = require('three-orbit-controls')(THREE)
 import GPUComuptationRenderer from '../lib/GPUComuptationRenderer'
+var glsl = require('glslify')
+const path = require('path');
 
 const WIDTH = window.innerWidth
 const HEIGHT = window.innerHeight
@@ -10,10 +12,23 @@ document.body.appendChild(renderer.domElement)
 
 const scene = new THREE.Scene()
 const camera = new THREE.PerspectiveCamera(75, WIDTH/HEIGHT, 1, 10000)
+camera.position.z = 500;
+
+var geometry = new THREE.CubeGeometry( 200, 200, 200 );
+
+var material = new THREE.ShaderMaterial({
+  vertexShader: glsl(path.resolve(__dirname, './shaders/vertex.glsl')),
+  fragmentShader: glsl(path.resolve(__dirname, './shaders/fragment.glsl'))
+});
+
+const cube = new THREE.Mesh(geometry, material );
+scene.add(cube)
 
 function render(t) {
-
+  renderer.render(scene, camera)
   requestAnimationFrame(render)
 }
+
+requestAnimationFrame(render)
 
 const control = new OrbitControls(camera, renderer.domElement)
